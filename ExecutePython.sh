fb8293160781e92c2abf025eb0fd120413b39ee1
architjhingan <architjhingan26@gmail.com>
2021-11-28 00:19:23 +0000

    ExecutePython Shell Script
    shell script to run all the python script

---
+++ ExecutePython.sh
@@ -0,0 +1,18 @@
+python control.py -p 3366 > control.log &
+sleep 20
+python Peer.py -s 3366 > peer1.log &
+sleep 20
+python Peer.py -s 3366 > peer2.log &
+sleep 20
+python Peer.py -s 3366 > peer3.log &
+sleep 20
+python Peer.py -s 3366 > peer4.log &
+sleep 20
+python Peer.py -s 3366 > peer5.log &
+sleep 20
+python Peer.py -s 3366 > peer6.log &
+sleep 20
+python Peer.py -s 3366 > peer7.log &
+sleep 20
+python Peer.py -s 3366 > peer8.log &
+
