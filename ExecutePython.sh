0823b1ae83adbb00acac3a68a81991ff3e4fd6f6
architjhingan <architjhingan26@gmail.com>
2021-11-28 17:13:05 +0000

    Update ExecutePython.sh
    updated now peers and running

--- ExecutePython.sh
+++ ExecutePython.sh
@@ -1,11 +1,11 @@
 python3 control.py -p 3365 > control.log &
-sleep 20
+sleep 20;
 python3 Peer.py -s 3365 > peer1.log &
-sleep 20
+sleep 20;
 python3 Peer.py -s 3365 > peer2.log &
-sleep 20
+sleep 20;
 python3 Peer.py -s 3365 > peer3.log &
-sleep 20
+sleep 20;
 python3 Peer.py -s 3365 > peer4.log &
-sleep 20
+sleep 20;
 python3 Peer.py -s 3365 > peer5.log &
