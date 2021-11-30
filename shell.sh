1cf67d1ca0db512a31fa5ab90be992787faaf526
Muvazima Mansoor <muvazima@Muvazimas-MacBook-Air.local>
2021-11-30 11:41:29 +0000

    shell

--- shell.sh
+++ shell.sh
@@ -1,12 +1,12 @@
 mkdir -p logs
 python3 index_server.py -p 33001 > logs/control.log &
 sleep 20
-python3 peer.py -s 33001 -p 33009 > logs/peer1.log &
+python3 Peer.py -s 33001 -p 33009 > logs/peer1.log &
 sleep 20
-python3 peer.py -s 33001 -p 33008 > logs/peer2.log &
+python3 Peer.py -s 33001 -p 33008 > logs/peer2.log &
 sleep 20
-python3 peer.py -s 33001 -p 33007 > logs/peer3.log &
+python3 Peer.py -s 33001 -p 33007 > logs/peer3.log &
 sleep 20
-python3 peer.py -s 33001 -p 33006 > logs/peer4.log &
+python3 Peer.py -s 33001 -p 33006 > logs/peer4.log &
 sleep 20
-python3 peer.py -s 33001 -p 33005 > logs/peer5.log &
+python3 Peer.py -s 33001 -p 33005 > logs/peer5.log &
